a048387c4f9c8ba3fa5beedef665a7d274b50df2
nomisal <tryeverythingican@gmail.com>
2025-04-21 19:00:47 +0800

    2025-04-21 clean

--- test/nomisal_res_test.dart
+++ test/nomisal_res_test.dart
@@ -1,16 +1,9 @@
-import 'package:nomisal_res/nomisal_res.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    setUp(() {});
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
